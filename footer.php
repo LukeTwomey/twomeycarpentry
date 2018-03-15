4dae3a8ad5abf424d6c5368eb3d93bdb3fa5275a
Luke Twomey <luke.twomey85@gmail.com>
2018-03-15 08:55:49 +0000

    Update footer details

--- footer.php
+++ footer.php
@@ -3,7 +3,12 @@
         <a href="http://www.facebook.com"><img src="images/icons/facebook.png"/></a>
         <a href="http://www.twitter.com"><img src="images/icons/twitter.png"/></a>
     </div>
+    <div class="contacts">
+    	<i class="fas fa-phone fa-sm"></i><a href="tel:+447923227280">07923 227 280</a>
+    	<br>
+    	<i class="fas fa-envelope fa-sm"></i><a href="mailto:info@twomeycarpentry.com">info@twomeycarpentry.com</a>
+    </div>
     <div class="copyright">
-        <p>Copyright &copy; Twomey Interiors 2016</p>
+        <p>Copyright &copy; Twomey Carpentry 2018</p>
     </div>
 </footer>
--- javascript-files.php
+++ javascript-files.php
@@ -3,4 +3,6 @@
 <script type="text/javascript" src="javascript/jquery.fancybox.pack.js"></script>
 <script src="https://npmcdn.com/imagesloaded@4.1/imagesloaded.pkgd.min.js"></script>
 <script src="https://npmcdn.com/masonry-layout@4.0/dist/masonry.pkgd.min.js"></script>
+<script defer src="javascript/fa-solid.min.js"></script>
+<script src="javascript/fontawesome.min.js"></script>
 <script type="text/javascript" src="javascript/javascript.js"></script>
