b48d0afce58683f65fe94777e4b53d2d53ca26fc
Luke Twomey <luke@luketwomey.com>
2018-03-18 14:59:15 +0000

    Combine and minimise JS. Tidy unwanted files

--- javascript-files.php
+++ javascript-files.php
@@ -1,8 +1,1 @@
-<script type="application/javascript" src="javascript/fastclick.js"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script type="text/javascript" src="javascript/jquery.fancybox.pack.js"></script>
-<script src="https://npmcdn.com/imagesloaded@4.1/imagesloaded.pkgd.min.js"></script>
-<script src="https://npmcdn.com/masonry-layout@4.0/dist/masonry.pkgd.min.js"></script>
-<script defer src="javascript/fa-solid.min.js"></script>
-<script src="javascript/fontawesome.min.js"></script>
-<script type="text/javascript" src="javascript/javascript.js"></script>
+<script type="text/javascript" src="javascript/min/javascript-min.js"></script>
