708a4af8b37a4798b48cec2a6fdc5e083dc7124f
Luke Twomey <luke@luketwomey.com>
2018-03-19 21:21:37 +0000

    Trim homepage CTA text

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
                     <div class="content">
                         <h1>Twomey Carpentry</h1>
                         <p>Welcome to Twomey Carpentry, the definitive choice for all of your carpentry and joinery needs.</p>
-                        <a href="about" class="button">Find out more about us</a>
+                        <a href="about" class="button">More about us</a>
                     </div>
                 </div>
 
@@ -32,7 +32,7 @@
                     <div class="content">
                         <h2>Experience</h2>
                         <p>With over forty years' experience in the industry, we have the unparalleled knowledge and expertise required to ensure your projects are completed to the very highest standard.</p>
-                        <a href="projects" class="button">View our previous projects</a>
+                        <a href="projects" class="button">Previous projects</a>
                     </div>
                 </div>
 
@@ -44,7 +44,7 @@
                         <h2>Services</h2>
                         <p>Covering Bexhill, Eastbourne and Hastings, we provide a wide range of services.<p> 
                         <p>From smaller jobs such as putting up shelves and hanging doors, to larger projects like kitchen fitting and flooring.</p>
-                        <a href="/" class="button">View services we provide</a>
+                        <a href="/" class="button">Services we provide</a>
                     </div>
                 </div>
 
