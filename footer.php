b3a9effd01dd1301e98548b56aaec854ad0389aa
Luke Twomey <luke@luketwomey.com>
2018-03-19 21:41:36 +0000

    Hide socials for now

--- footer.php
+++ footer.php
@@ -1,8 +1,8 @@
 <footer>
-    <div class="social">
+    <!-- <div class="social">
         <a href="http://www.facebook.com"><img src="images/icons/facebook.png"/></a>
         <a href="http://www.twitter.com"><img src="images/icons/twitter.png"/></a>
-    </div>
+    </div> -->
     <div class="contacts">
     	<i class="fas fa-phone fa-sm"></i><a href="tel:+447923227280">07923 227 280</a>
     	<br>
